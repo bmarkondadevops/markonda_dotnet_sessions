b7d6506bb617d92cd265b8878c6ea1ce114bc047
Markonda Reddy <markonda.reddy@rrd.com>
2025-01-09 09:35:50 +0530

    updated

---
+++ day21/CircleApp1/CircleApp1/Models/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace CircleApp1.Models
+{
+    internal class Circle
+    {
+        private float radius;
+        public float Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+
+        public float CalculateArea()
+        {
+            return (float)Math.PI * (radius * radius);
+        }
+    }
+}
---
+++ day21/CircleApp1/CircleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using CircleApp1.Models;
+
+namespace CircleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Create a new Circle object
+            Circle circle = new Circle();
+            circle.Radius = 5.0f;
+
+            // Calculate and display the area
+            float area = circle.CalculateArea();
+            Console.WriteLine($"The area of the circle with radius {circle.Radius} is {area}");
+
+        }
+    }
+}
