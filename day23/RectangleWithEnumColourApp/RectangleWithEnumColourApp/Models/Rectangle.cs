bb6a37f60750243f83e44c6c45086115bce20dd7
Markonda Reddy <markonda.reddy@rrd.com>
2025-01-16 07:42:48 +0530

    updated

---
+++ day23/RectangleWithEnumColourApp/RectangleWithEnumColourApp/Models/Rectangle.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleWithEnumColourApp.Models
+{
+    internal class Rectangle
+    {
+        //Declare fields
+        private int _width;
+        private int _height;
+        private Colours _colour;
+        private const int MAX_VALUE = 100;
+        private const int MIN_VALUE = 1;
+
+        public int Width
+        {
+            get {
+                return _width;
+            }
+            set {
+                _width = IsValidate(value);
+            }
+        }
+
+        public int Height
+        {
+            get {
+                return _height;
+            }
+            set {
+                _height = IsValidate(value);
+            }
+        }
+
+        public Colours Colour
+        {
+            get {
+                return _colour;
+            }
+            set {
+                _colour = value;
+            }
+        }
+
+        private int IsValidate(int value)
+        {
+            if (value < MIN_VALUE)
+            {
+                return value = MIN_VALUE;
+            }
+            if (value > MAX_VALUE)
+            {
+                return value = MAX_VALUE;
+            }
+            return value;
+        }
+
+        //Area of Rectangle
+        public int Area
+        {
+            get
+            {
+                return _width * _height;
+            }
+        }
+
+
+    }
+}
---
+++ day23/RectangleWithEnumColourApp/RectangleWithEnumColourApp/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+using System;
+using RectangleWithEnumColourApp.Models;
+
+
+namespace RectangleWithEnumColourApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Rectangle r1 = new Rectangle();
+            r1.Width = 5;
+            r1.Height = 13;
+            r1.Colour = Colours.Green;
+
+            Rectangle r2 = new Rectangle();
+            r1.Width = 6;
+            r1.Height = 18;
+            r1.Colour = Colours.Red;
+            
+            Rectangle r3 = new Rectangle();
+            r1.Width = 8;
+            r1.Height = 12;
+            r1.Colour = Colours.Yellow;
+
+            Rectangle r4 = new Rectangle();
+            r1.Width = 4;
+            r1.Height = 30;
+            r1.Colour = Colours.Orange;
+
+            Rectangle[] rectangles = new Rectangle[4];
+            rectangles[0] = r1;
+            rectangles[1] = r2;
+            rectangles[2] = r3;
+            rectangles[3] = r4;
+
+            Console.WriteLine("Size of rectangle is:" + rectangles.Length);
+
+            PrintDetails(rectangles);
+        }
+
+        private static void PrintDetails(Rectangle[] rectangles)
+        {
+            foreach (Rectangle rectangle in rectangles)
+            {
+                PrintDetails("manyRectangles", rectangle);
+            }
+        }
+
+        private static void PrintDetails(string details, Rectangle rectangle)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine($"Printing details of : {details}  foreground colour is {rectangle.Colour}");
+            Console.WriteLine($"width {rectangle.Width} , height is {rectangle.Height}, Area is {rectangle.Area} ,Colour is {rectangle.Colour}");
+            Console.WriteLine();
+            Console.ResetColor();
+        }
+    }
+}
