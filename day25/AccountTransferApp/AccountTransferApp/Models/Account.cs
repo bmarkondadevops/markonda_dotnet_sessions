b9bb3d72e1c49964e391efc24a807422ef060bfe
Markonda Reddy <markonda.reddy@rrd.com>
2025-01-20 08:45:25 +0530

    day25

---
+++ day25/AccountTransferApp/AccountTransferApp/Models/Account.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountTransferApp.Models
+{
+    internal class Account
+    {
+
+        private string _accountNumber; 
+        private double _balance;
+
+        public string AccountNumber
+        {
+            get
+            {
+                return _accountNumber;
+            }
+        }
+
+        public double Balance
+        {
+            get
+            {
+                return _balance;
+            }
+        }
+
+        public Account(string accountNumber, double initialBalance)
+        {
+            _accountNumber = accountNumber;
+            _balance = initialBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                _balance += amount;
+                Console.WriteLine($"Deposited {amount} into account {AccountNumber}. New balance: {Balance}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid deposit amount.");
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && amount <= _balance)
+            {
+                _balance -= amount;
+                Console.WriteLine($"Withdrew {amount} from account {AccountNumber}. New balance: {Balance}");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds or invalid withdrawal amount.");
+            }
+
+        }
+
+
+        public void DoTransfer(Account destinationAccount, double amount)
+        {
+            if (amount > 0 && amount <= _balance)
+            {
+                Withdraw(amount);
+                destinationAccount.Deposit(amount);
+                Console.WriteLine($"Transferred {amount} from account {AccountNumber} to account {destinationAccount.AccountNumber}.");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds or invalid transfer amount.");
+            }
+        }
+    }
+}
---
+++ day25/AccountTransferApp/AccountTransferApp/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using AccountTransferApp.Models;
+
+namespace AccountTransferApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Create two accounts
+            var fromAccount = new Account("9966636824", 1000.00);
+            var toAccount = new Account("9790604084", 500.00);
+
+            // Display initial balances
+            Console.WriteLine($"Account {fromAccount.AccountNumber} balance: {fromAccount.Balance}");
+            Console.WriteLine($"Account {toAccount.AccountNumber} balance: {toAccount.Balance}");
+
+            // Transfer money from acc1 to acc2
+            fromAccount.DoTransfer(toAccount, 500.00);
+
+            // Display final balances
+            Console.WriteLine($"Account {fromAccount.AccountNumber} balance after transfer: {fromAccount.Balance}");
+            Console.WriteLine($"Account {toAccount.AccountNumber} balance after transfer: {toAccount.Balance}");
+        }
+    }
+}
