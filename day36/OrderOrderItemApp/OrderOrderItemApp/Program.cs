2d3ab5e287943cf227eb14c860e26d2e9a1093e0
Markonda Reddy <markonda.reddy@rrd.com>
2025-02-04 10:04:08 +0530

    updated

--- day36/OrderOrderItemApp/OrderOrderItemApp/Program.cs
+++ day36/OrderOrderItemApp/OrderOrderItemApp/Program.cs
@@ -7,8 +7,8 @@ namespace OrderOrderItemApp
         static void Main(string[] args)
         {
             // Create some sample order items
-            var item1 = new OrderItem("Product A", 2, 10.99);
-            var item2 = new OrderItem("Product B", 3, 5.99);
+            var item1 = new OrderItem("Pens", 2, 10.99);
+            var item2 = new OrderItem("Books", 3, 5.99);
 
             // Create an order
             var order1 = new Order(1, "John Doe");
